2c611b5e7c5380a8e0cddf7d694ee574d326ed74
efochesatto <eliasfochesatto@gmail.com>
2019-10-29 23:54:16 -0300

    Alterando subtração, para gerar quantidade de primos

--- subtracao.py
+++ subtracao.py
@@ -1,4 +1,17 @@
-n1=int(input("Informe o primeiro número: "))
-n2=int(input("Informe o segundo número: "))
-resultado=n1-n2
-print(resultado) 
+a = int (input("Digite o valor de ‘A’: "))
+b = int (input("Digite o valor de ‘B’: "))
+primos = a - b
+encontrados=0
+testando=2
+while encontrados < primos:
+    testador=1
+    conta_div=0
+    while testando >= testador:
+        if testando%testador == 0:
+            conta_div+=1
+        testador+=1
+    if conta_div == 2:
+        print (testando,"",end="")
+        encontrados+=1
+    testando+=1
+print("")
