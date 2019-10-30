d55f0b81f31b18b677aac69e8c78dd6355c5357b
efochesatto <eliasfochesatto@gmail.com>
2019-10-29 23:30:38 -0300

    Alterando arquivo soma, para verificar primalidade do resultado

--- soma.py
+++ soma.py
@@ -1,4 +1,12 @@
-n1=int(input("Informe o primeiro número: "))
-n2=int(input("Informe o segundo número: "))
-resultado=n1+n2
-print(resultado) 
+a=int(input("Informe o primeiro número: "))
+b=int(input("Informe o segundo número: "))
+numero = a+b
+if numero > 1:
+    for i in range (2, numero):
+        if (numero % i) == 0:
+            print ("O número",numero,"não é primo.")
+            break
+    if (i == (numero - 1)):
+        print ("O número",numero,"é primo.")
+else:
+    print ("O número",numero,"não é primo.")
