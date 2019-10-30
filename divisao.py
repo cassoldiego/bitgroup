ef33f31e4a2709d5674ff2015f92509b66b7c053
efochesatto <eliasfochesatto@gmail.com>
2019-10-29 23:14:54 -0300

    Alterando arquivo divisão, para reaizar impressão dos 'muitos'

--- divisao.py
+++ divisao.py
@@ -1,5 +1,12 @@
-
-n1=int(input("Informe o primeiro número: "))
-n2=int(input("Informe o segundo número: "))
-resultado=n1/n2
-print(resultado) 
+a = int (input("Digite o valor de ‘A’: "))
+b = int (input("Digite o valor de ‘B’: "))
+resultado = a//b
+cont1 = cont2 = 1
+for i in range (0, resultado):
+      print ("O curso é ", end = "")
+      while (cont2 >= 1):
+         print ("muito ", end = "")
+         cont2 = cont2 - 1
+      print ("bom!")
+      cont1 +=1
+      cont2 = cont1
