fcb8dd36c8837d6e6f3e64c55a4da45047320d13
efochesatto <eliasfochesatto@gmail.com>
2019-10-29 23:23:00 -0300

    Alterando arquivo multiplica para impressão de quadrado de asteriscos

--- multiplica.py
+++ multiplica.py
@@ -1,6 +1,12 @@
-
 # Multiplicando dois números
-
-a = int (input("Insira o primeiro valor: "))
-b = int (input("Insira o segundo valor: "))
-print ("Resultado é: ", a * b)
+# Gerando quadrado com o tamanho do produto
+ 
+a=int(input("Informe o primeiro número: "))
+b=int(input("Informe o segundo número: "))
+tamanho = a*b
+for c in range (0, tamanho):
+    for i in range (0, tamanho):
+        if (i == (tamanho - 1)):
+            print ("*")
+        else:
+            print("* ", end="")
